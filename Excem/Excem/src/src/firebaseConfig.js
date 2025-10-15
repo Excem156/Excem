410f542793bdaf1ab4b1120dd7ab77da2621abc7
Excem156 <worldwideexcem@gmail.com>
2025-10-15 18:54:30 +0100

    Create firebaseConfig.js

---
+++ Excem/Excem/src/src/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEsJLeMuz3JMS4L9rbjcnNK7S1nXzigqc",
+  authDomain: "excem-ce973.firebaseapp.com",
+  projectId: "excem-ce973",
+  storageBucket: "excem-ce973.appspot.com",
+  messagingSenderId: "411960390993",
+  appId: "1:411960390993:web:7c702ff2cdcb8b70da4f70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Authentication and Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
