298b430f0d1afb6b53ef448b9558a27dfe9a9be1
Excem156 <worldwideexcem@gmail.com>
2025-10-15 18:24:12 +0100

    Create App.js

---
+++ Excem/Excem/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Excem 🌍</h1>
+      <p>Your web app is now live!</p>
+    </div>
+  );
+}
+
+export default App;
