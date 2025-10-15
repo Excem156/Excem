915f0d10b8e9bd153dca9837d5a71f73a4c52341
Excem156 <worldwideexcem@gmail.com>
2025-10-15 18:56:20 +0100

    Create App.js

---
+++ Excem/Excem/src/src/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>🚀 Welcome to Excem!</h1>
+      <p>Your Firebase connection is live.</p>
+    </div>
+  );
+}
+
+export default App;
