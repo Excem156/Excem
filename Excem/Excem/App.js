7c1d21a0eaae27420cc9d227e2598bf2865c2987
Excem156 <worldwideexcem@gmail.com>
2025-10-15 16:06:54 +0100

    Create App.js

---
+++ Excem/Excem/App.js
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Dashboard from "./components/pages/Dashboard";
+import PhoneAuth from "./components/pages/PhoneAuth";
+import SocialAuth from "./components/pages/SocialAuth";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/phone" element={<PhoneAuth />} />
+        <Route path="/social" element={<SocialAuth />} />
+      </Routes>
+    </Router>
+  );
+}
