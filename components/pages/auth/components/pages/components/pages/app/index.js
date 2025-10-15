8ec851c7d0eb3a6634752bb15374c178fae3cc16
Excem156 <worldwideexcem@gmail.com>
2025-10-15 15:18:22 +0100

    Create index.js

---
+++ components/pages/auth/components/pages/components/pages/app/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Main entry point for Excem App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
