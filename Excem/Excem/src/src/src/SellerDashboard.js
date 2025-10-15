ba1bc1931729a5a9931394a4f68eb417bf07c3a8
Excem156 <worldwideexcem@gmail.com>
2025-10-15 19:49:46 +0100

    Create SellerDashboard.js

---
+++ Excem/Excem/src/src/src/SellerDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SellerDashboard() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h2>Seller Dashboard</h2>
+      <p>Welcome, Seller! You can add, edit, or remove your products here.</p>
+    </div>
+  );
+}
+
+export default SellerDashboard;
