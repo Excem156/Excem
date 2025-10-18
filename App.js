1fb54817839f8fa0a63ac9599aeb4158b4b02e50
Excem156 <worldwideexcem@gmail.com>
2025-10-18 18:46:08 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Signup from "./Signup";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>🚀 Welcome to Excem</h1>
+      <p>Your Vercel + Firebase + Vite app is working!</p>
+    </div>
   );
 }
 
