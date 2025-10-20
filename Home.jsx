594b908710951d7a4660e76281dfd21956be5a07
Excem156 <worldwideexcem@gmail.com>
2025-10-20 21:17:34 +0100

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Signup from "./Signup";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function App() {
+function Home() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <div style={{ textAlign: 'center', padding: '40px' }}>
+      <h1>Welcome to EXCEM 🌍</h1>
+      <p>Your trusted marketplace for buying and selling safely worldwide.</p>
+
+      <div style={{ marginTop: '20px' }}>
+        <Link
+          to="/shop"
+          style={{
+            backgroundColor: '#0c9248',
+            color: '#fff',
+            padding: '10px 20px',
+            borderRadius: '8px',
+            textDecoration: 'none',
+            fontWeight: 'bold'
+          }}
+        >
+          Go to Shop
+        </Link>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default Home;
