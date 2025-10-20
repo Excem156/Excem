6302515a99227050c70c963fde965a918ff17bed
Excem156 <worldwideexcem@gmail.com>
2025-10-20 11:58:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// This is the entry point of your web app.
+// It renders your main App component (where all pages connect).
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
