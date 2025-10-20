18f1c71e515b7bc1f3e79d32176061a9d1700cb5
Excem156 <worldwideexcem@gmail.com>
2025-10-20 20:04:33 +0100

    Fix ReactDOM rendering for Vite

--- index.jsx
+++ index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App'
 
+// Create the React root and render the App
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
