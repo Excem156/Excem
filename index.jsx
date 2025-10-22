d39d5663bc6f4d0e9da12e2e79f4c07cdbb8b580
Excem156 <worldwideexcem@gmail.com>
2025-10-22 17:47:15 +0100

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+// Ensure this path is correct for your AuthContext file
+import { AuthProvider } from './contexts/AuthContext'; 
 
-// Create a root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthProvider> {/* <-- WRAPPER ADDED */}
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 );
